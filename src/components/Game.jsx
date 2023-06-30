9770130f8f9d6c338850c27b9c548cbd90849ae4
Maéva Lagrost <johndoe@exemple.com>
2023-06-30 20:58:50 +0200

    add hook useGameOver

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Menu } from "../components/Menu";
 
+// Hooks
+import { useGameOver } from "../hooks/useGameOver";
+
 export const Game = ({ rows, columns }) => {
   const start = () => {
-    console.log("start");
+    console.log(`Start gameOver is ${gameOver}`);
   };
+
+  const [gameOver, setGameOver, resetGameOver] = useGameOver("");
   return (
     <div className="Game">
       <Menu onClick={start} />
